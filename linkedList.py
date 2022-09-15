91922a8b505fb9c5f9d195be0a4d0bdd34da9065
Nikita Chevychelov <86499004+chevychelovN@users.noreply.github.com>
2022-09-15 13:53:50 +0300

    Created simple list implementation

---
+++ linkedList.py
@@ -0,0 +1,124 @@
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked List is empty")
+            return
+
+        itr = self.head
+        llstr = ''
+
+        while itr:
+            llstr += str(itr.data) + "-->"
+            itr = itr.next
+
+        print(llstr)
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+
+        return count
+
+    def remove_at(self, index):
+        if index < 0 or index >= self.get_length():
+            raise Exception("Invalid index")
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+
+    def insert_at(self, index, data):
+        if index < 0 or index >= self.get_length():
+            raise Exception("Invalid index")
+
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next
+            count += 1
+
+    def insert_after_value(self, data_after, data_to_insert):
+        itr = self.head
+        while itr:
+            if itr.data == data_after:
+                node = Node(data_to_insert, itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next
+            if itr is None:
+                raise Exception("There is no such value")
+
+    def remove_by_value(self, data):
+        itr = self.head
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+
+        while itr:
+            if itr.next.data == data:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            if itr is None:
+                raise Exception("There is no such value")
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert_values([1, 2, 3, 4])
+    ll.insert_at(0, 5)
+    ll.insert_at(2, 6)
+    ll.print()
+    ll.remove_by_value(5)
+    ll.print()
